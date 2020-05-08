9e118db07a678d3f3c8567e8e7583754c3159e56
余方杨 <a975057282@gmail.com>
2020-05-08 14:02:25 +0800

    console

--- src/providers/redis/redis.module.ts
+++ src/providers/redis/redis.module.ts
@@ -3,6 +3,8 @@ import { ORMCONFIG } from '@/app.config';
 import { RedisModule as _RedisModule } from 'nestjs-redis';
 import { DynamicModule } from '@nestjs/common';
 
+console.info(REDIS);
+
 export const RedisModule: DynamicModule = _RedisModule.register({
   host: REDIS.host,
   port: REDIS.port,
